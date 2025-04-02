fa10c3c8b628e8b7cfcee7f66f612814addf7ad7
bigth21 <taekhyeon.nam@gmail.com>
2025-04-02 14:40:11 +0900

    Add CSS related

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
+import styles from "./page.module.css"
 
 export default function BlogLayout({children}: {children: React.ReactNode}) {
-    return <section>{children}</section>
+    return <section className={styles.blog}>{children}</section>
 };
